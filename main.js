79fccc858a50629b4962a4a3c614e36166ec44ce
HannahGloria <eleonor.ragl@gmail.com>
2020-12-22 20:36:14 -0600

    git init

---
+++ main.js
@@ -0,0 +1,11 @@
+
+  $('.sweet-home').slick({
+    dots: true,
+    infinite: true,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    adaptiveHeight: false
+  });
+              
